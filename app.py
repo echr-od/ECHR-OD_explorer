c8591d91578b60550c68229db761d27a4f6258c2
Alexandre Quemy <alexandre.quemy@gmail.com>
2019-09-26 13:17:37 +0200

    Initial skeleton

---
+++ app.py
@@ -0,0 +1,60 @@
+from fastapi import FastAPI
+
+from starlette.applications import Starlette
+from starlette.staticfiles import StaticFiles
+from starlette.responses import HTMLResponse
+from starlette.templating import Jinja2Templates
+import uvicorn
+
+
+templates = Jinja2Templates(directory='templates')
+
+app = FastAPI(debug=True)
+app.mount('/static', StaticFiles(directory='statics'), name='static')
+app.mount('/assets', StaticFiles(directory='statics/assets'), name='assets')
+
+
+@app.route('/')
+async def homepage(request):
+    template = "index.html"
+    context = {"request": request}
+    return templates.TemplateResponse(template, context)
+
+
+@app.route('/cases/001-60803')
+async def homepage(request):
+    template = "case.html"
+    context = {"request": request}
+    return templates.TemplateResponse(template, context)
+
+
+@app.route('/error')
+async def error(request):
+    """
+    An example error. Switch the `debug` setting to see either tracebacks or 500 pages.
+    """
+    raise RuntimeError("Oh no")
+
+
+@app.exception_handler(404)
+async def not_found(request, exc):
+    """
+    Return an HTTP 404 page.
+    """
+    template = "404.html"
+    context = {"request": request}
+    return templates.TemplateResponse(template, context, status_code=404)
+
+
+@app.exception_handler(500)
+async def server_error(request, exc):
+    """
+    Return an HTTP 500 page.
+    """
+    template = "500.html"
+    context = {"request": request}
+    return templates.TemplateResponse(template, context, status_code=500)
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host='0.0.0.0', port=8000)
